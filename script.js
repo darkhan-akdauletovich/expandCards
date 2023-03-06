4ebd967ddd52947f71890caa6dac967b14828a2a
darkhan-akdauletovich <d.bolatov2402@gmail.com>
2023-03-06 15:09:32 +0600

    add commentaries

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const panels = document.querySelectorAll('.panel')
 //Document method querySelectorAll() returns a static (not live) NodeList representing a list of the document's elements that match the specified group of selectors.
 
+/*Static NodeLists
+In other cases, the NodeList is static, where any changes in the DOM do not affect the content of the collection. The ubiquitous document.querySelectorAll() method returns a static NodeList. */
+
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
