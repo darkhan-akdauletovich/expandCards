dc7a6c396bdc2bf5a9d33fb08ff4c45522c655d1
darkhan-akdauletovich <d.bolatov2402@gmail.com>
2023-03-06 15:02:02 +0600

    add commentaries

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const panels = document.querySelectorAll('.panel')
+//Document method querySelectorAll() returns a static (not live) NodeList representing a list of the document's elements that match the specified group of selectors.
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
